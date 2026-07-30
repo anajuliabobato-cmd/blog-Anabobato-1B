a95096cf19bcf25318bf1dd51015858f0e4988b0
anajuliabobato-cmd <ana.julia.bobato@escola.pr.gov.br>
2026-07-30 12:34:17 +0000

    v6

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // cria um constante que seleciona o botão no html
 const botoes = document.querySelectorAll("button");
 botoes.forEach(function(botao){ 
+    let curtiu = false;
 botao.addEventListener("click", botaoClicado); 
 
 function botaoClicado() {
 console.log("fui clicado");
 let texto = botao.querySelector("span");
-texto.textContent++;
+if (curtiu === false){
+    texto.textContent++;
+    curtiu = true;
+}
+else{
+    texto.textContent--;
+    curtiu = false;
+}
 }
 });
 
